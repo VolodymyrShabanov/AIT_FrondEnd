9a254fcd8bd710410c84eff145c477e7af875b2e
Volodymyr Shabanov <volodymyrshw@gmail.com>
2024-01-23 16:23:39 +0100

    add hw_11

---
+++ home_work_11/index.js
@@ -0,0 +1,57 @@
+// console.log("Hello. HW_11");
+
+const caseForm = document.getElementById("form");
+const caseList = document.getElementById("caseList");
+const btnDelLast = document.getElementById("btnDelLast");
+const btnClearList = document.getElementById("btnClearList");
+
+// console.log(caseForm, caseList, btnClearList, btnDelLast);
+
+function clearInputs(event) {
+  event.target.caseTitle.value = "";
+  event.target.descript.value = "";
+}
+
+function changeStatus(event) {
+  if (event.target.style.textDecoration === "line-through") {
+    event.target.style.textDecoration = "none";
+  } else {
+    event.target.style.textDecoration = "line-through";
+  }
+}
+
+function clearList() {
+  while (caseList.hasChildNodes()) {
+    caseList.firstChild.remove();
+  }
+}
+
+btnClearList.addEventListener("click", clearList);
+
+function delLast() {
+  if (caseList.hasChildNodes()) {
+    caseList.lastChild.remove();
+  }
+}
+
+btnDelLast.addEventListener("click", delLast);
+
+caseForm.addEventListener("submit", (event) => {
+  event.preventDefault(); //отключаем обновление страницы
+
+  const caseTitle = {
+    caseTitle: event.target.caseTitle.value,
+    descript: event.target.descript.value,
+  };
+
+  const textItem = document.createElement("p");
+  textItem.innerText = `Title: ${event.target.caseTitle.value} |=| Description: ${event.target.descript.value};`;
+
+  textItem.style.cursor = "pointer";
+  textItem.style.color = "purple";
+
+  textItem.onclick = changeStatus; // ! почему????? не передаем ничего и функция работает
+
+  caseList.append(textItem);
+  clearInputs(event);
+});
